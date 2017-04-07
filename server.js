5401310d31ca885d0ad84f70627b02b95e49b468
arshdkhn1 <arshdkhn1@gmail.com>
2017-04-08 00:20:33 +0530

    module completed

--- server.js
+++ server.js
@@ -3,25 +3,15 @@ require('babel-register')({
 });
 
 const express = require('express');
-const app = express();
 const compression = require('compression');
-const React = require('react');
-const ReactDOMServer = require('react-dom/server');
 const path = require('path');
+
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const Component = require('./Component');
-
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  const props = { title: 'Universal React' };
-  const html = ReactDOMServer.renderToString(
-    React.createElement(Component, props)
-  );
-  res.send(html);
-});
+app.use(require('./routes/index'));
 
 app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}!`));
